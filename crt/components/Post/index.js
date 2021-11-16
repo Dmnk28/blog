f1bff391444a470830465baed4d64a63ae5a5fc4
Dominik Oesterle <dominik.oesterle@posteo.de>
2021-11-16 16:50:09 +0100

    Implemented a MarkdownRendering Method via Arrays
    Found already a better solution, but it worked pretty fine and was my very own work. So I pushed it, to remember my learnings.

--- crt/components/Post/index.js
+++ crt/components/Post/index.js
@@ -1,20 +1,53 @@
+
 import { Box, Paper, Typography } from "@mui/material";
 import Image from 'next/image';
-import ReactMarkdown from "react-markdown";
-import ReactMarkdownRenderers from "@utils/ReactMarkdownRenderers";
+import marked from 'marked';
+
 
 export default function Post (props) {
     const {post} = props;
+    
+    const stations = [
+        {
+            html: "h1",
+            content: 'Test'
+        },
+        {
+            html: "h2",
+            content: 'Test'
+        }
+    ];
+
+    const Test = (markedLexerOutput) => (
+        <div>
+          {markedLexerOutput.map((object, index) => {
+              switch(object.type) {
+                    case "paragraph":
+                        return (<Typography variant="body1" key={object.type + index.toString()}>{object.text}</Typography>)
+                    case "heading":
+                        if (object.depth >= 5) return (<Typography variant="body1" key={object.type + index.toString()}><strong>{object.text}</strong></Typography>);
+                        let newDepth = object.depth + 2;
+                        return (<Typography variant={"h" + newDepth.toString()} component={"h" + object.depth.toString()} gutterBottom key={object.type + index.toString()}>{object.text}</Typography>)
+                    default:
+                        return;
+              }
+            })}
+        </div>
+      );
+    
+    
+      const lexerText = '## This will be a Markdown H2';
+      const lexed = marked.lexer(post.content);
+      console.log(lexed, Test(lexed));
+
     return (
         <Box mt={2} >
             <Paper elevation={6}>
                 <Typography variant="h2" component="h1">
                     {post.title}
                 </Typography>
-
-                <ReactMarkdown components={ReactMarkdownRenderers(post.content)}>
-                    {post.content}
-                </ReactMarkdown>
+                               
+                {Test(lexed)}
 
             </Paper>
         </Box>
