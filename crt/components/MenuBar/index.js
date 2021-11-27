fb05ca366d0c44e4085de048e3741ce01c3748e0
Dominik Oesterle <dominik.oesterle@posteo.de>
2021-11-27 13:58:07 +0100

    Changed the Mobile menu to a Modal-Component

--- crt/components/MenuBar/index.js
+++ crt/components/MenuBar/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 import NextLink from 'next/link'
-import { AppBar, Box, IconButton, Link as MUILink, Popover, Toolbar, Typography } from '@mui/material';
+import { AppBar, Avatar, Box, Button, Divider, IconButton, Link as MUILink, List, ListItemButton, ListItemAvatar, ListItemText, Paper, Toolbar, Typography } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu'
+import Modal from '@mui/material/Modal'
 
 import Brightness2 from '@mui/icons-material/Brightness2';
 import Brightness7 from '@mui/icons-material/Brightness7';
+import CloseIcon from '@mui/icons-material/Close';
 import MuiNextButton from '@components/MuiNextButton';
 
 export default function MenuBar({mode, setMode}) {
-    const [burgerPopoverAnchor, setBurgerPopoverAnchor] = React.useState(null);
+    const [burgerOpen, setBurgerOpen] = React.useState(false);
 
     const menuItemsArray = [
+        {
+            name: 'Start',
+            href: '/'
+        },
         {
             name: 'Code',
             href: '/code',
@@ -29,16 +35,8 @@ export default function MenuBar({mode, setMode}) {
         },
     ]
 
-    const isBurgerOpen = Boolean(burgerPopoverAnchor);
-
-    const handleBurgerClick = (event) => {
-        setBurgerPopoverAnchor(event.currentTarget)
-    }
-    const handleBurgerPopoverClose = () => {
-        setBurgerPopoverAnchor(null);
-    }
-
-
+    const handleBurgerOpen = () => setBurgerOpen(true); 
+    const handleBurgerClose = () => setBurgerOpen(false);
 
     const handleModeBtn = () => {
         if (mode === 'light') {
@@ -61,41 +59,71 @@ export default function MenuBar({mode, setMode}) {
                     {menuItemsArray.map((element, index) => (
                         <MuiNextButton key={element.name + index.toString()} href={element.href} color="inherit" btnText={element.name} deactivate={false}/>
                     ))}
-                    <IconButton onClick={handleModeBtn} color="inherit">
+                    <Button onClick={handleModeBtn} color="inherit">
                         {mode === 'light' ? <Brightness2 /> : <Brightness7 />}
-                    </IconButton>
+                    </Button>
                 </Box>
                 
-                <IconButton
+                <Button
                     id="burgerButton"
                     size="large"
                     edge="start"
                     color="inherit"
                     aria-label="burgermenu"
-                    aria-describedby="burgerMenu"
-                    onClick={handleBurgerClick}
+                    onClick={handleBurgerOpen}
                     sx={{ display: {xs: 'flex', md: 'none'}}}
                 >
                     <MenuIcon />
-                </IconButton>
-                <Popover 
-                        id="burgerMenu"
-                        open={isBurgerOpen}
-                        anchorEl={burgerPopoverAnchor}
-                        onClose={handleBurgerPopoverClose}
-                        anchorOrigin={{
-                            vertical: "bottom",
-                            horizontal: "center",
-                        }}
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right'
-                        }}
+                </Button>
+                <Modal 
+                        open={burgerOpen}
+                        onClose={handleBurgerClose}
+                        aria-label="Mobile Menu"
                 >
-                    {menuItemsArray.map((element, index) => (
-                        <MuiNextButton key={element.name + index.toString()} href={element.href} color="inherit" btnText={element.name} deactivate={false}/>
-                    ))}       
-                </Popover>
+                    <Paper sx={{
+                                position: 'absolute',
+                                top: '50%',
+                                left: '50%',
+                                transform: 'translate(-50%, -50%)',
+                                boxShadow: '24',
+                                minWidth: '70%',
+                                p: 1
+                            }}
+                    >
+                        <Box sx={{display: 'flex',justifyContent: "end"}}>
+                            <IconButton onClick={handleBurgerClose} 
+                                    aria-label="close" 
+                                    color="secondary"
+                                    edge="start"
+                            >
+                                <CloseIcon />
+                            </IconButton>
+                        
+                        </Box>
+                        
+                        <List>
+                        {menuItemsArray.map((element, index) => (
+                            <React.Fragment key="test" key={element.name + index.toString()}>
+                                <NextLink href={element.href} passHref>
+                                    <ListItemButton onClick={handleBurgerClose} component="a">
+                                        <ListItemText primary={element.name}/>
+                                    </ListItemButton>
+                                </NextLink>
+                                <Divider />
+                            </React.Fragment>
+                        ))}
+                            
+                            <ListItemButton sx={{mt:2}} onClick={handleModeBtn} color="inherit">
+                                <ListItemAvatar>
+                                    <Avatar sx={{bgcolor: mode === 'light' ? 'primary' : ''}}>
+                                    { mode === 'light' ? <Brightness2 color="secondary"/> : <Brightness7 color="secondary"/>}
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary={ mode === 'light' ? 'Dark Mode' : 'Light Mode' }/>    
+                            </ListItemButton>
+                        </List>
+                    </Paper>       
+                </Modal>
             </Toolbar>
         </AppBar>
     );
