71b96c94a4fbd4f9fa326321fd71e1fbb74c0825
Dominik Oesterle <dominik.oesterle@posteo.de>
2021-12-04 16:17:06 +0100

    Made the Footer larger with margin and padding

--- crt/components/Footer/index.js
+++ crt/components/Footer/index.js
@@ -1,12 +1,15 @@
 import NextLink from 'next/link';
 import { Link as MUILink } from '@mui/material'
+import { Box } from '@mui/system';
 
 export default function Footer () {
     return (
         <footer id="footer">
-            <NextLink href="https://do-webdev.de/projects/timer" passHref>
-                <MUILink variant="body" color="secondary">&copy; 2021 Dominik Oesterle</MUILink>
-            </NextLink>
+            <Box sx={{my: 6, p: 9,}}>
+                <NextLink href="https://do-webdev.de/projects/timer" passHref>
+                    <MUILink variant="body" color="primary">&copy; 2021 Dominik Oesterle</MUILink>
+                </NextLink>
+            </Box>
         </footer>
     );
 }
